617baf91c9aee0160eb851de12f08d43465a85f2
李立波 <llb421270473@163.com>
2017-04-18 17:24:30 +0800

    调整首页展示情况

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,6 @@
 class MoviesController < ApplicationController
   def index
     page = params[:page]
-    fetch_res = Spider::FetchResource.new
-    fetch_res.fetch_movies(1)
-    @movies = MovieStore.page(page)
+    @movies = MovieStore.order(id: :asc).page(page)
   end
 end
