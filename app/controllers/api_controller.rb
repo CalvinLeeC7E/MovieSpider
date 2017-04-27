610df3fd62235faa038bcfb3110ce1b199e7d06c
李立波 <llb421270473@163.com>
2017-04-27 11:46:42 +0800

    电影接口返回数据时添加豆瓣评论

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,7 +2,24 @@ class ApiController < ApiApplicationController
 
   api_get :movie do
     page = params[:page]
-    MovieStore.order(order_level: :desc).page(page)
+    MovieStore.includes(:dou_ban_movie_rating).order(order_level: :desc).page(page).map do |item|
+      rating = {}
+      if item.dou_ban_movie_rating && item.dou_ban_movie_rating.stars.present?
+        rating = {
+            rating: {
+                stars: item.dou_ban_movie_rating.stars,
+                avg_rating: item.dou_ban_movie_rating.avg_rating,
+                collect_count: item.dou_ban_movie_rating.collect_count,
+                from: '豆瓣'
+            }
+        }
+      end
+      {
+          title: item.title,
+          img_cover: item.img_cover,
+          des: '',
+      }.merge rating
+    end
   end
 
   api_get :douban_search do
--- app/models/movie_store.rb
+++ app/models/movie_store.rb
@@ -1,4 +1,7 @@
 class MovieStore < ActiveRecord::Base
+
+  has_one :dou_ban_movie_rating, class_name: 'DouBanMovieRating', foreign_key: 'movie_store_id'
+
   def have_update_douban
     self.update_attribute :dou_ban_update_time, Time.now
   end
